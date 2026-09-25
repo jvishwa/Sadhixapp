d23a1d257b5f83989a50bc4c92a21e88f1b209b4
jvishwa <88563096+jvishwa@users.noreply.github.com>
2026-09-25 20:03:17 +0530

    Update database host in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000; // You can change the port if needed
 // Establishing a connection with the database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '10.160.0.4',
   user: 'root',
   password: 'Password@1234',
   database: 'sadhixdb'
